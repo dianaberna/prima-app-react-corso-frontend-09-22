d3a33a6038deada2cc74b50722529c69f3bacefd
dianaberna <bernabei.d@gmail.com>
2022-09-26 12:04:58 +0200

    aggiornati componenti

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -4,14 +4,20 @@ import Form from "../Form/Form"
 import ButtonLoader from "../ButtonLoader/ButtonLoader"
 
 export default function Main(){
+    let numero = 10 
+    let array = []
+    for (let i = 1; i < numero; i++) {
+        array.push(<Button key={i}>{i}</Button>)
+    }
     return (
         <div>
             sono il main
-            
             <br/>
             <Button type="secondary">testo del bottone</Button>
             <ButtonLoader>Invia</ButtonLoader>
             <Form/>
+            <h2>stampo {numero} bottoni</h2>
+            {array}
         </div>
     )
 }
